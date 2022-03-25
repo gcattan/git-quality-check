967075eb163676770d57c23d91347cf79b5d563f
gcattan <gregoire.cattan@ibm.com>
2022-03-25 21:27:29 +0100

    [Refactor] Code clean and improve naming of function. Factorize some of the code.

--- git-quality-check.py
+++ git-quality-check.py
@@ -1,20 +1,27 @@
+from re import L
 import subprocess
 from datetime import datetime
 import random
 
 bad_words = ["WIP", "work in progress", "in progress", "TODO"]
+main_branches = ["origin/develop", "origin/master"]
+
+def run_git(command:list[str]):
+    command.insert(0, "--no-pager")
+    command.insert(0, "git")
+    return subprocess.check_output(command).decode()
+
 def git_logs():
-    ret = subprocess.check_output(["git", "--no-pager", "log"]).decode()
-    ret = ret.split("commit ")
-    return ret
+    return run_git(["log"]).split("commit ")
 
 def remove_first_line(log:str):
-    if(is_valid_log):
+    if is_valid_log:
         try:
             eol = log.index("\n")
             log = log[eol+1:]
         except ValueError:
-            return "" #commit is empty or just contains one line
+            #commit is empty or just contains one line
+            return "" 
     return log
 
 def is_valid_log(log:str):
@@ -29,18 +36,22 @@ def process_logs(logs: list[str], functions: list[str: int]):
             counter += function(log)
     return counter / count * 100
 
-def is_empty_body(log:str):
-    if not is_valid_log(log):
-        return 1
+def remove_header(log:str):
     log = remove_first_line(log) # Hash
     log = remove_first_line(log) # Author
     log = remove_first_line(log) # Date
+    return log
+
+def is_empty_body(log:str):
+    if not is_valid_log(log):
+        return 1
+    log = remove_header(log)
     if not is_valid_log(log):
          return 1
     return 0
 
 ####################################
-def no_squashed_commit(log):
+def not_a_squashed_commit(log):
     if not "(#" in log:
         return 1
     return 0
@@ -60,19 +71,20 @@ def is_test_commit(log:str):
 
 ####################################
 
+def strip(s:str):
+    return s.strip().lstrip()
+
 def git_all_branches():
-    ret = subprocess.check_output(["git", "--no-pager", "branch", "-r"]).decode()
-    return [r.strip().lstrip() for r in ret.split("\n")]
+    ret = run_git(["branch", "-r"]).split("\n")
+    return [strip(r) for r in ret]
 
 def git_get_branch_date(branch):
-    if("->" in branch):
+    if "->" in branch:
         return None
-    ret = subprocess.check_output(["git", "log", "-n", "1", "--pretty=%as", branch]).decode()
-    ret = ret.split("-")
+    ret = run_git(["log", "-n", "1", "--pretty=%as", branch]).split("-")
     return datetime(int(ret[0]), int(ret[1]), int(ret[2]))
 
 def get_date():
-    # date = datetime.today().strftime('%Y-%m-%d')
     return datetime.today()
 
 def diff_month(d1, d2):
@@ -85,69 +97,54 @@ def is_old(branch):
     date = get_date()
     return diff_month(date, branch_date) > 2
 
-def count_old_branches(branches):
-    count = len(branches)
-    counter = 0
-    i = 0
-    min = 10
+def sample(l:list[str], min:int):
+    count = len(l)
     if(count > min):
-        branches = random.sample(branches, min)
+        l = random.sample(branches, min)
         count = min
+    return (l, count)
+
+def count_old_branches(branches):
+    counter = 0
+    branches, count = sample(branches, 10)
     for branch in branches:
-        i += 1
-        # print(i / count * 100)
         counter += 1 if is_old(branch) else 0
     return counter / count * 100
 
 def are_coupled(branchA:str, branchB:str):
-    # print(branchA, branchB)
     if branchA == branchB:
         return False
-    ret = subprocess.check_output(["git", "--no-pager", "branch", "--contains", branchA, "-r"]).decode().split("\n")
+    ret = run_git(["branch", "--contains", branchA, "-r"]).split("\n")
     for r in ret:
         if branchB == r.strip().lstrip():
-            # print("found", branchB)
             return True
     return False
 
 
 def count_coupled(branches):
-    count = len(branches)
-    min = 10
-    if(count > min):
-        branches = random.sample(branches, min)
-        count = min
-    branches.append("origin/develop")
-    branches.append("origin/master")
-    # branches.append("origin/main")
-    count += 2
+    branches, count = sample(branches, 10)
+    branches.extend(main_branches)
+    count += len(main_branches)
     counter = 0
-    i = 0
     for bA in branches:
         for bB in branches:
-            i += 1
-            # print(i / count**2 * 100)
             counter = 1 if are_coupled(bA, bB) else 0
     return counter / count * 100
 
 logs = git_logs()
-bad_commit_index = process_logs(logs, [no_squashed_commit, is_empty_body, count_bad_words])
+branches = git_all_branches()
+
+bad_commit_index = process_logs(logs, [not_a_squashed_commit, is_empty_body, count_bad_words])
 test_index = process_logs(logs, [is_test_commit])
 
+old_branches_index = count_old_branches(branches)
+coupling_index = count_coupled(branches)
 
-  
-branches = git_all_branches()
-branches,
-bA = branches[0]
-bB = branches[1]
-old_branches = count_old_branches(branches)
-# coupling = count_coupled(branches)
-coupling = 0
 
 print(bad_commit_index)
 print(test_index)
-print(old_branches)
-print(coupling)
+print(old_branches_index)
+print(coupling_index)
 
 # bad_commit_index = 0
 # test_index = 100
@@ -155,8 +152,8 @@ print(coupling)
 # coupling = 0
 
 overall = (100 - bad_commit_index) + \
- test_index + (100 - old_branches) + \
-     (100 - coupling)
+ test_index + (100 - old_branches_index) + \
+     (100 - coupling_index)
 
 print(overall / 4)
 
