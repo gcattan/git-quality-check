30bbde65ca33fd4b9e9020685d68b40692b97ad2
gcattan <gregoire.cattan@ibm.com>
2022-03-28 20:18:38 +0200

    escape %as

--- git-quality-check.py
+++ git-quality-check.py
@@ -90,7 +90,7 @@ def git_all_branches():
 def git_get_branch_date(branch):
     if "->" in branch:
         return None
-    ret = run_git(["log", "-n", "1", "--pretty=%as", branch]).split("-")
+    ret = run_git(["log", "-n", "1", "--pretty=\"%as\"", branch]).split("-")
     print(ret)
     return datetime(int(ret[0]), int(ret[1]), int(ret[2]))
 
