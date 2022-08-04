6b48e94b1f7280541dd6f0907ee34f995200080b
gcattan <gregoire.cattan@ibm.com>
2022-08-04 19:04:31 +0200

    test process logs: GREEN

--- tests/indicators/counters/test_process_logs.py
+++ tests/indicators/counters/test_process_logs.py
@@ -1,5 +1,18 @@
 from git_quality_check.indicators.counters import process_logs
+from git_quality_check.indicators.commits import not_a_squashed_commit, is_empty_body
 
 
 def test():
-    assert True == False
+    logs = ["""Commit 1234
+    Author: 
+    Date:
+    Test""",
+    """Commit 1234
+    Author: 
+    Date:
+    Merge (#5)""",
+    ""]
+    marked_commits = process_logs(
+        logs, [not_a_squashed_commit, is_empty_body]
+    )
+    assert marked_commits == 50
