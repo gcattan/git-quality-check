cc3b74d37667a3d2aca4283172bf132db39fd56f
gcattan <gcattan@hotmail.fr>
2022-08-04 17:59:36 +0200

    test for is_empty_body

--- tests/indicators/commits/test_is_empty_body.py
+++ tests/indicators/commits/test_is_empty_body.py
@@ -1,5 +1,14 @@
 import pytest
+from git_quality_check.indicators.commits import is_empty_body
+
+@pytest.mark.parametrize(
+        'log', ['', 'commit 1234455']
+    )
+def test_is_empty_body(log):
+    assert is_empty_body(log) == 1
 
 
-def test():
-    assert True == False
+def test_is_not_empty_body():
+    log = """commit 1234455
+    Test"""
+    assert is_empty_body(log) == 0
