5dd5a11490e11e4a61db5b51376ca1530a2d8ebc
gcattan <gcattan@hotmail.fr>
2022-08-04 18:01:06 +0200

    typo

--- tests/indicators/commits/test_is_empty_body.py
+++ tests/indicators/commits/test_is_empty_body.py
@@ -9,6 +9,8 @@ def test_is_empty_body(log):
 
 
 def test_is_not_empty_body():
-    log = """commit 1234455
-    Test"""
+    log = """commit 7a1e2a6a76e6967bde14e95900996ca17811de47 (origin/gc/pytest)
+    Author: gcattan <gregoire.cattan@ibm.com>
+    Date:   2022-04-17
+    remove dependencies"""
     assert is_empty_body(log) == 0
