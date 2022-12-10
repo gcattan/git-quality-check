3ce38b3e29e7e10ece333c1f4640b7746e681cbb
gcattan <gcattan@hotmail.fr>
2022-12-10 21:18:48 +0100

    Add some recommendation in example

--- example/git-quality-check.py
+++ example/git-quality-check.py
@@ -39,9 +39,15 @@ if __name__ == "__main__":
     coupling_index = count_coupled(branches, main_branches)
 
     print("Percent of bad commits: ", format_number(bad_commit_index))
+    print("    A high number of bad commits means you should avoid WIP or empty commits for example.")
     print("Percent of test commits: ", format_number(test_index))
+    print("    A low number of test commits, might indicates that you are not writing enough tests for your software.")
     print("Percent of old branches: ", format_number(old_branches_index))
+    print("    A high percent of old branches, means that you should delete unused branches or already merged branches in your repo.")
     print("Percent of coupled branches", format_number(coupling_index))
+    print("    A high percent of coupled branches, means that you should use more often squash \
+        and merge and avoid upmerge of your master/main branch in feature branches.\
+        A high number of coupled branches complicate your commit history and make difficult the use of git-bisect for example.")
 
     overall = compute_score(
         bad_commit_index, test_index, old_branches_index, coupling_index
