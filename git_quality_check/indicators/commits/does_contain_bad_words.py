eadcf92e866c5b99fae38126f31de7d8a946d98f
gcattan <gcattan@hotmail.fr>
2022-12-10 12:59:54 +0100

    Update does_contain_bad_words.py

--- git_quality_check/indicators/commits/does_contain_bad_words.py
+++ git_quality_check/indicators/commits/does_contain_bad_words.py
@@ -1,20 +1,20 @@
-"""
-Fixture returning an handler that tell if a log contains
-the specified bad words.
-
-Parameters
-----------
-bad_words : list[str]
-    A list of word to be avoided in a commit log.
-
-Returns
- -------
-_does_contain_bad_words: Callable(log: str)
-    Handler method that return true if the log contains the specified
-    "bad" words.
-  
-"""
 def does_contain_bad_words(bad_words: list[str]):
+    """
+    Fixture returning an handler that tell if a log contains
+    the specified bad words.
+
+    Parameters
+    ----------
+    bad_words : list[str]
+        A list of word to be avoided in a commit log.
+
+    Returns
+     -------
+    _does_contain_bad_words: Callable(log: str)
+        Handler method that return true if the log contains the specified
+        "bad" words.
+
+    """
     def _does_contain_bad_words(log: str):
         counter = 0
         for word in bad_words:
