7a6740d1ba51c0db4bc82d08df79d42f3c5ac269
gcattan <gcattan@users.noreply.github.com>
2022-12-10 19:49:18 +0000

    :art: Format Python code with psf/black

--- git_quality_check/indicators/counters/process_logs.py
+++ git_quality_check/indicators/counters/process_logs.py
@@ -1,25 +1,25 @@
 def process_logs(logs: list[str], functions: list[str:int]):
     """
-    Call a list of functions on the commit logs.
-    Each function is a scoring function that returns
-    either 1 or 0.
-    The method then return the percent of functions having returned 1.
+        Call a list of functions on the commit logs.
+        Each function is a scoring function that returns
+        either 1 or 0.
+        The method then return the percent of functions having returned 1.
 
->>>>>>> d427c6eba7617351a44a3bf759bdb38e9b3ce25d
-    Parameters
-    ----------
-    logs: list[str]
-        The commit logs
-    functions: list[str:int]
-        A list of scoring functions, taking a commit log as a parameter
-        and returning either 1 or 0.
-        Note, it is expected that all scoring functions have the same `meaning`.
-        That is, they all return 1 (or 0) when the logs is validated (or unvalidated).
+    >>>>>>> d427c6eba7617351a44a3bf759bdb38e9b3ce25d
+        Parameters
+        ----------
+        logs: list[str]
+            The commit logs
+        functions: list[str:int]
+            A list of scoring functions, taking a commit log as a parameter
+            and returning either 1 or 0.
+            Note, it is expected that all scoring functions have the same `meaning`.
+            That is, they all return 1 (or 0) when the logs is validated (or unvalidated).
 
-    Returns
-     -------
-    percent: float
-        The percent of functions having returned 1.
+        Returns
+         -------
+        percent: float
+            The percent of functions having returned 1.
     """
     counter = 0
     count = len(logs) * len(functions)
