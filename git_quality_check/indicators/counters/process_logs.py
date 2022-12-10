5a69c714a0339662af3e0447708dfafd1e6cf417
gcattan <gcattan@hotmail.fr>
2022-12-10 20:50:48 +0100

    fix previous merge conflict

--- git_quality_check/indicators/counters/process_logs.py
+++ git_quality_check/indicators/counters/process_logs.py
@@ -3,13 +3,12 @@ def process_logs(logs: list[str], functions: list[str:int]):
     Call a list of functions on the commit logs.
     Each function is a scoring function that returns
     either 1 or 0.
-    The method then return the percent of functions having returned 1.
+    The method then returns the percent of functions having returned 1.
 
->>>>>>> d427c6eba7617351a44a3bf759bdb38e9b3ce25d
     Parameters
     ----------
     logs: list[str]
-        The commit logs
+        The commit logs.
     functions: list[str:int]
         A list of scoring functions, taking a commit log as a parameter
         and returning either 1 or 0.
