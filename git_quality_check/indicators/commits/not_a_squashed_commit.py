bb96a90209caea2ef5f20f66268d6da5464ede57
gcattan <gcattan@hotmail.fr>
2022-12-10 16:44:42 +0100

    comment for not_a_squased_commit

--- git_quality_check/indicators/commits/not_a_squashed_commit.py
+++ git_quality_check/indicators/commits/not_a_squashed_commit.py
@@ -1,4 +1,18 @@
 def not_a_squashed_commit(log: str):
+    """
+    Return true if the commit is not a squashed commit.
+    (ie. it was not squased and merged from a PR).
+
+    Parameters
+    ----------
+    log : str
+        The log of a commit.
+
+    Returns
+     -------
+    is_not_a_squashed_commit: Bool
+        true if the commit is NOT a squashed commit.
+    """
     if "(#" not in log:
         return 1
     return 0
